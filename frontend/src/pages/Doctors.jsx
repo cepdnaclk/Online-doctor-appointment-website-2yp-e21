d8cf953da036fcae90924f3c0b093a73bdadab94
Kaveesha-Madhushan17 <e21245@eng.pdn.ac.lk>
2025-10-22 09:21:33 +0530

    Intiate the bacekend and crete the doctor shema and model

--- frontend/src/pages/Doctors.jsx
+++ frontend/src/pages/Doctors.jsx
@@ -7,6 +7,7 @@ const Doctors = () => {
   const {speciality} = useParams()
   const navigate = useNavigate();
   const [filterDoc, setFilterDoc] = useState([])
+  const [showFilter,setShowFilter] = useState(false)
   const {doctors} = useContext(AppContext)
 
 
@@ -24,8 +25,9 @@ const Doctors = () => {
   return (
     <div>
     <p className='text-gray-600'>Filter by Speciality</p>
+    <button onClick={()=>setShowFilter(prev=>!prev)} className={`py-1 px-3 border rounded text-sm transition-all sm:hidden ${showFilter?'bg-primary-200 text-white':''}`}>Filter</button>
     <div className='flex flex-col sm:flex-row items-start gap-5 mt-5' >
-      <div className='flex flex-col gap-4 text-sm text-gray-600'>
+      <div className={`flex-col gap-4 text-sm text-gray-600 ${showFilter?'flex' :'hidden sm:flex'}`}>
         <p onClick={()=>speciality === 'General physician' ? navigate('/doctors') : navigate('/doctors/General physician')} className={`w-[94vw] sm:w-auto pl-3 py-1.5 pr-16 border border-gray-300 rounded-xl transition-all cursor-pointer ${speciality === "General physician" ? "bg-primary-50 text-black" : ""}`}>General physician</p> {/*conditionally styling here*/}
         <p onClick={()=>speciality === 'Gynecologist' ? navigate('/doctors') : navigate('/doctors/Gynecologist')} className={`w-[94vw] sm:w-auto pl-3 py-1.5 pr-16 border border-gray-300 rounded-xl transition-all cursor-pointer ${speciality === "Gynecologist" ? "bg-primary-50 text-black" : ""}`}>Gynecologist</p>
         <p onClick={()=>speciality === 'Dermatologist' ? navigate('/doctors') : navigate('/doctors/Dermatologist')} className={`w-[94vw] sm:w-auto pl-3 py-1.5 pr-16 border border-gray-300 rounded-xl transition-all cursor-pointer ${speciality === "Dermatologist" ? "bg-primary-50 text-black" : ""}`}>Dermatologist</p>
