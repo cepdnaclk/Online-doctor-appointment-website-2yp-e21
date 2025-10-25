23724a5dfd3752afc925550fcbb2cf53f5e40b0a
Shamiska <e21077@eng.pdn.ac.lk>
2025-10-25 08:19:19 +0530

    Removed the dummy values and connected the database with the homepage

---
+++ backend/routes/adminRoute.js
@@ -0,0 +1,17 @@
+import express from 'express'
+import { addDoctor,allDoctors,loginAdmin } from '../controllers/adminController.js'
+import upload from '../middlewares/multer.js'
+import authAdmin from '../middlewares/authAdmin.js'
+
+const adminRouter = express.Router()
+
+// to add doctors
+adminRouter.post('/add-doctor',authAdmin ,upload.single('image'), addDoctor)
+
+// to register
+adminRouter.post('/login', loginAdmin)
+
+//to get all doctors
+adminRouter.post('/all-doctors', authAdmin,allDoctors)
+
+export default adminRouter
--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -5,30 +5,46 @@ import { Link, NavLink, useNavigate } from 'react-router-dom'
 const Navbar = () => {
     const navigate = useNavigate();
 
-    //const [showMenu, setShowMenu] = useState(false);
+    const [showMenu, setShowMenu] = useState(false);
     const [token, setToken] = useState(true);
     
 
 return (
     // this is the navigation bar
     <div className='flex  justify-between items-center py-1'>
-            <Link to='/'><img src={assets.logo} alt="" class='w-30 cursor-pointer'/></Link>
-            <ul class='flex gap-8 items-center'>
+            <Link to='/'><img src={assets.logo} alt="" className='w-50 cursor-pointer'/></Link>
+            <ul className='flex gap-8 items-center'>
                 <NavLink to='/'>
-                    <li> HOME </li>
-                    <hr  class='boarder-none outline-none h-0.5 bg-primary w-5/5 m-auto hidden'/>
+                    {({isActive}) => (
+                        <>
+                            <li className={`hover:text-primary hover:drop-shadow-[0_0_8px_rgba(176,24,231,0.6)] transition-all duration-300 ${isActive ? 'text-primary-200 font-semibold' : 'text-gray-700'}`}> HOME </li>
+                            <hr className={`border-none outline-none h-0.5 bg-primary-200 w-full m-auto ${isActive ? 'block' : 'hidden'}`}/>
+                        </>
+                    )}
                 </NavLink>
                 <NavLink to='/doctors'>
-                    <li>DOCTORS</li>
-                    <hr  class='boarder-none outline-none h-0.5 bg-primary w-5/5 m-auto hidden'/>
+                    {({isActive}) => (
+                        <>
+                            <li className={`hover:text-primary hover:drop-shadow-[0_0_8px_rgba(176,24,231,0.6)] transition-all duration-300 ${isActive ? 'text-primary-200 font-semibold' : 'text-gray-700'}`}>DOCTORS</li>
+                            <hr className={`border-none outline-none h-0.5 bg-primary-200  w-full m-auto ${isActive ? 'block' : 'hidden'}`}/>
+                        </>
+                    )}
                 </NavLink>
                 <NavLink to='/about'>
-                    <li>ABOUT</li>
-                    <hr  class='boarder-none outline-none h-0.5 bg-primary w-5/5 m-auto hidden'/>
+                    {({isActive}) => (
+                        <>
+                            <li className={`hover:text-primary hover:drop-shadow-[0_0_8px_rgba(176,24,231,0.6)] transition-all duration-300 ${isActive ? 'text-primary-200 font-semibold' : 'text-gray-700'}`}>ABOUT</li>
+                            <hr className={`border-none outline-none h-0.5 bg-primary-200 w-full m-auto ${isActive ? 'block' : 'hidden'}`}/>
+                        </>
+                    )}
                 </NavLink>
                 <NavLink to='/contact'>
-                    <li>CONTACT</li>
-                    <hr  class='boarder-none outline-none h-0.5 bg-primary w-5/5 m-auto hidden'/>
+                    {({isActive}) => (
+                        <>
+                            <li className={`hover:text-primary hover:drop-shadow-[0_0_8px_rgba(176,24,231,0.6)] transition-all duration-300 ${isActive ? 'text-primary-200 font-semibold' : 'text-gray-700'}`}>CONTACT</li>
+                            <hr className={`border-none outline-none h-0.5 bg-primary-200 w-full m-auto ${isActive ? 'block' : 'hidden'}`}/>
+                        </>
+                    )}
                 </NavLink>
             </ul>
             <div class='flex items-center gap-4 '>{
@@ -47,7 +63,22 @@ return (
                         </div>
                 </div>
                 :<button onClick = {()=>navigate('/login')} class='border border-primary text-primary py-2 px-6 rounded-full hover:bg-primary hover:text-white transition-all duration-20 md:block'>Create account</button>
-}</div>
+}
+            <img onClick={()=> setShowMenu(true)} src={assets.menu_icon} alt="" className='w-6 md:hidden' />
+            {/* ----Mobile menu----*/}
+            <div className={`${showMenu ? 'fixed w-full' : 'h-0 w-0'}  md:hidden right-0 top-0 bottom-0 z-20 overflow-hidden bg-white transition-all duration-500`}>
+                <div className='flex items-center justify-between px-5 py-6'>
+                    <img className='w-36' src={assets.logo} alt="" />
+                    <img className='w-7' onClick={()=> setShowMenu(false)} src={assets.cross_icon} alt="" />
+                </div>
+                <ul className='flex flex-col items-center gap-2 mt-5 px-5 text-lg font-medium'>
+                    <NavLink    onClick={()=> setShowMenu(false)}  to='/'><p className='px-4 py-2 rounded inline-block'>HOME</p></NavLink>
+                    <NavLink  onClick={()=> setShowMenu(false)}  to='/doctors'><p className='px-4 py-2 rounded inline-block'>ALL DOCTORS</p></NavLink>
+                    <NavLink   onClick={()=> setShowMenu(false)} to='/about'><p className='px-4 py-2 rounded inline-block'>ABOUT</p></NavLink>
+                    <NavLink  onClick={()=> setShowMenu(false)}  to='/contact'><p className='px-4 py-2 rounded inline-block'>CONTACT</p></NavLink>
+                </ul>
+            </div>
+</div> 
 
     </div>
 )
